1641f4ce2d805e586ee03ed2ab37529a6359a93b
Fantezievfx <exoticraul798@gmail.com>
2026-01-30 23:17:22 +0000

    Fix callback URL

--- server.js
+++ server.js
@@ -69,8 +69,7 @@ passport.use(
     {
       clientID: process.env.DISCORD_CLIENT_ID,
       clientSecret: process.env.DISCORD_CLIENT_SECRET,
-      callbackURL:
-        process.env.DISCORD_CALLBACK_URL || 'http://localhost:3000/auth/discord/callback',
+      callbackURL: process.env.DISCORD_CALLBACK_URL,
       scope: ['identify']
     },
     (accessToken, refreshToken, profile, done) => {
